e33dd9d0dd0a387e46c9fb1b3a8113525961820a
John Norris <johnanthonynorris@googlemail.com>
2012-01-08 01:21:18 +0000

    productmanager.addProductInstance wasn't workin propa

--- resources/js/productset.productManager.js
+++ resources/js/productset.productManager.js
@@ -24,15 +24,15 @@ MultiProductDetailManager.prototype.getProductObjectByUniqueId = function( produ
 MultiProductDetailManager.prototype.addProductObject = function( productDetailObject ){
 	
 	//check if it exists:
-	if( !this.getProductObjectByUniqueId( productDetailObject.configObjArea["objProductUniqueId"] ) ){
+	//if( !this.getProductObjectByUniqueId( productDetailObject.configObjArea["objProductUniqueId"] ) ){
 		
 		//return instance
 		this.productDetailObjects.push( productDetailObject );
 		
-	}else{
+	//}else{
 		
 		//add to array of instances
-		return this.getProductObjectByUniqueId( productDetailObject.configObjArea["objProductUniqueId"] );
-	}
+	//	return this.getProductObjectByUniqueId( productDetailObject.configObjArea["objProductUniqueId"] );
+	//}
 	
 };
